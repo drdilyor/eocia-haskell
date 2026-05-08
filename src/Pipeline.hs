7311b4d08141380326da62bff71d001d1919e808
drdilyor <drdilyor@outlook.com>
2026-05-08 18:21:28 +0200

    add: liveness analysis of blocks and jumps

--- src/Pipeline.hs
+++ src/Pipeline.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 module Pipeline where
 
-import Data.Foldable
+import Data.Foldable qualified as Foldable
 import Data.HashMap.Strict qualified as Map
 import Data.HashSet qualified as Set
 import Data.Int
@@ -203,6 +203,7 @@ selectInstructions ss = fmap reverse $ execState [] $ siStmt ss
   siStmt (Return e) = do
     emit
       [ Movq (Reg Rax) (siArg e)
+      , Jmp "main_conclusion"
       ]
   siStmt (Goto label) = do
     emit [ Jmp label ]
@@ -212,11 +213,11 @@ data StackFrame = StackFrame
   , size :: Int
   }
 
-assignHomes :: forall es. [AsmVar] -> Eff es (Int, [Asm])
+assignHomes :: forall es. Cfg [AsmVar] -> Eff es (Int, Cfg [Asm])
 assignHomes asmvar = mdo
   (program, StackFrame{size}) <-
     runState (StackFrame Map.empty 0) do
-      mapM instruction asmvar
+      traverse (traverse instruction) asmvar
   pure (size, program)
  where
   instruction :: AsmVar -> Eff (State StackFrame : es) Asm
@@ -254,62 +255,112 @@ assignHomes asmvar = mdo
 type LivenessItem = Either Reg Text
 type LivenessTrace = NonEmpty (Set.HashSet LivenessItem)
 
-uncoverLive :: [AsmVar] -> LivenessTrace
-uncoverLive [] = NE.singleton Set.empty
-uncoverLive (inst : rest) =
-  let restLiveness@(after :| _) = uncoverLive rest
-      before = (after `Set.difference` write inst) `Set.union` read' inst
-   in before `NE.cons` restLiveness
+uncoverLive :: Cfg [AsmVar] -> Cfg ([AsmVar], LivenessTrace)
+buildInterferenceGraph :: Cfg ([AsmVar], LivenessTrace) -> Set.HashSet (LivenessItem, LivenessItem)
+(uncoverLive, buildInterferenceGraph) = (uncoverLive, buildInterferenceGraph)
+ where
+  -- perhaps use Phases functor
+  uncoverLive Cfg {root, blocks} = Cfg {root, blocks = go $ topsort root}
+   where
+    go [] = Map.empty
+    go (v : (go -> rest)) = Map.insert v (blocks Map.! v, uncoverBlock rest $ blocks Map.! v) rest
 
--- TODO: somehow refactor
--- TODO: Deref is completely broken because deref actually reads even if it is dst
-arg' :: Arg a v -> Maybe LivenessItem
-arg' (Var x) = Just (Right x)
--- maybe we should check for the speciality here, though doing so breaks killing the register when written, in `write`
-arg' (Reg x) = Just (Left x)
-arg' (Deref _ _) = error "TODO"
-arg' _ = Nothing
+    topsort :: Label -> [Label]
+    topsort v | Nothing <- blocks Map.!? v = []
+    topsort v = foldl1' (\a b -> nubOrd (a <> b)) (map topsort $ jumps $ blocks Map.! v)
+    jumps =
+      nubOrd . foldMap \case
+        Jmp label -> [label]
+        Jc _ label -> [label]
+        _ -> []
 
-arg :: Arg a v -> Set.HashSet LivenessItem
-arg = Set.fromList . maybeToList . arg'
+    uncoverBlock _ [] = NE.singleton Set.empty
+    uncoverBlock blocks (inst : rest) =
+      let restLiveness@(after :| _) = uncoverBlock blocks rest
+          beforeIfContinue = (after `Set.difference` write inst) `Set.union` read' inst
+          before =
+            case inst of
+              Jmp label -> NE.head (snd $ blocks Map.! label)
+              Jc _ label -> NE.head (snd $ blocks Map.! label) <> beforeIfContinue
+              _ -> beforeIfContinue
+       in before `NE.cons` restLiveness
+  buildInterferenceGraph = Foldable.fold . fmap (uncurry doBlock)
+   where
+    doBlock asm (_ :| liveness) | length asm /= length liveness = error "buildInterferenceGraph: invalid list sizes"
+    doBlock asm (_ :| liveness) =
+      -- ignore special registers while building the graph,
+      -- therefore we don't need to ignore them while doing colorGraph
+      let instruction (Movq dst src) after = instructionMov dst src after
+          instruction (Movzbqal dst) after = instructionMov dst (Reg Rax) after
+          instruction i after =
+            Set.fromList
+              [ edge
+              | v <- Set.toList after
+              , d <- Set.toList (write i)
+              , v /= d
+              , not (isSpecial v || isSpecial d)
+              , edge <- [(v, d), (d, v)]
+              ]
+          instructionMov (arg' -> Just d) (arg' -> s) after =
+            Set.fromList
+              [ edge
+              | v <- Set.toList after
+              , v /= d && v `notElem` s
+              , not (isSpecial v || isSpecial d)
+              , edge <- [(v, d), (d, v)]
+              ]
+          instructionMov _ _ _ = error "infallible"
+       in Set.unions (zipWith instruction asm liveness)
 
-write :: AsmVar -> Set.HashSet LivenessItem
-write (Movq a _) = arg a
-write (Movzbqal a) = arg a
-write (Addq a _) = arg a
-write (Subq a _) = arg a
-write (Negq a) = arg a
-write (Andq a _) = arg a
-write (Orq a _) = arg a
-write (Xorq a _) = arg a
-write (Cmpq _ _) = mempty
-write (Setal _) = Set.singleton (Left Rax)
-write (Jc _ _) = mempty
-write (Jmp _) = mempty  -- ?
-write (Pushq _) = mempty
-write (Popq a) = arg a
-write (Callq _) = Set.fromList $ Left <$> returnRegs
-write Retq = mempty
+  -- TODO: somehow refactor
+  -- TODO: Deref is completely broken because deref actually reads even if it is dst
+  arg' :: Arg a v -> Maybe LivenessItem
+  arg' (Var x) = Just (Right x)
+  -- maybe we should check for the speciality here, though doing so breaks killing the register when written, in `write`
+  arg' (Reg x) = Just (Left x)
+  arg' (Deref _ _) = error "TODO"
+  arg' _ = Nothing
 
-read' :: AsmVar -> Set.HashSet LivenessItem
-read' (Movq _ b) = arg b
-read' (Movzbqal _) = Set.singleton (Left Rax)
-read' (Addq a b) = arg a <> arg b
-read' (Subq a b) = arg a <> arg b
-read' (Negq a) = arg a
-read' (Andq a b) = arg a <> arg b
-read' (Orq a b) = arg a <> arg b
-read' (Xorq a b) = arg a <> arg b
-read' (Cmpq a b) = arg a <> arg b
-read' (Setal _) = mempty
-read' (Jc _ _) = mempty
-read' (Jmp _) = mempty
-read' (Pushq a) = arg a
-read' (Popq _) = mempty
-read' (Callq "input_int") = Set.empty
-read' (Callq "print_int") = Set.fromList [Left Rdi]
-read' (Callq _) = Set.fromList $ Left <$> argumentRegs
-read' Retq = mempty
+  arg :: Arg a v -> Set.HashSet LivenessItem
+  arg = Set.fromList . maybeToList . arg'
+
+  write :: AsmVar -> Set.HashSet LivenessItem
+  write (Movq a _) = arg a
+  write (Movzbqal a) = arg a
+  write (Addq a _) = arg a
+  write (Subq a _) = arg a
+  write (Negq a) = arg a
+  write (Andq a _) = arg a
+  write (Orq a _) = arg a
+  write (Xorq a _) = arg a
+  write (Cmpq _ _) = mempty
+  write (Setal _) = Set.singleton (Left Rax)
+  write (Jc _ _) = error "write: doesn't support jumps"
+  write (Jmp _) = error "write: doesn't support jumps"
+  write (Pushq _) = mempty
+  write (Popq a) = arg a
+  write (Callq _) = Set.fromList $ Left <$> returnRegs
+  write Retq = mempty
+
+  read' :: AsmVar -> Set.HashSet LivenessItem
+  read' (Movq _ b) = arg b
+  read' (Movzbqal _) = Set.singleton (Left Rax)
+  read' (Addq a b) = arg a <> arg b
+  read' (Subq a b) = arg a <> arg b
+  read' (Negq a) = arg a
+  read' (Andq a b) = arg a <> arg b
+  read' (Orq a b) = arg a <> arg b
+  read' (Xorq a b) = arg a <> arg b
+  read' (Cmpq a b) = arg a <> arg b
+  read' (Setal _) = mempty
+  read' (Jc _ _) = error "read': doesn't support jumps"
+  read' (Jmp _) = error "read': doesn't support jumps"
+  read' (Pushq a) = arg a
+  read' (Popq _) = mempty
+  read' (Callq "input_int") = Set.empty
+  read' (Callq "print_int") = Set.fromList [Left Rdi]
+  read' (Callq _) = Set.fromList $ Left <$> argumentRegs
+  read' Retq = mempty
 
 {- | special registers always map to the same register after assignRegisters.
 
@@ -318,31 +369,6 @@ note that assignRegisters also rewrites the registers, not just vars
 isSpecial :: LivenessItem -> Bool
 isSpecial (Left r) = r `elem` argumentRegs || r `elem` returnRegs
 isSpecial (Right _) = False
-
-buildInterferenceGraph :: [AsmVar] -> LivenessTrace -> Set.HashSet (LivenessItem, LivenessItem)
-buildInterferenceGraph asm (_ :| liveness) | length asm /= length liveness = error "buildInterferenceGraph: invalid list sizes"
-buildInterferenceGraph asm (_ :| liveness) =
-  let instruction (Movq (arg' -> Just d) (arg' -> s)) after =
-        Set.fromList
-          [ edge
-          | v <- Set.toList after
-          , v /= d && v `notElem` s
-          , -- ignore special registers while building the graph,
-          -- therefore we don't need to ignore them while doing colorGraph
-          not (isSpecial v || isSpecial d)
-          , edge <- [(v, d), (d, v)]
-          ]
-      instruction (Movq _ _) _ = error "infallible"
-      instruction i after =
-        Set.fromList
-          [ edge
-          | v <- Set.toList after
-          , d <- Set.toList (write i)
-          , v /= d
-          , not (isSpecial v || isSpecial d)
-          , edge <- [(v, d), (d, v)]
-          ]
-   in Set.unions (zipWith instruction asm liveness)
 
 -- | if a node has no edges, then it won't be present in the graph. in that case, the color can simply be defaulted to 0
 colorGraph :: Set.HashSet (LivenessItem, LivenessItem) -> Map.HashMap LivenessItem Int
@@ -379,9 +405,9 @@ colorGraph g =
           (OrdSet.fromList $ (0,) <$> vertices)
           (Map.fromList $ (,mempty) <$> vertices)
 
-assignRegisters :: [AsmVar] -> [AsmVar]
+assignRegisters :: Cfg [AsmVar] -> Cfg [AsmVar]
 assignRegisters asm =
-  let graph = buildInterferenceGraph asm (uncoverLive asm)
+  let graph = buildInterferenceGraph (uncoverLive asm)
       colors = colorGraph graph
       registers = [Rcx, Rdx, Rbx]
       assignRegister :: LivenessItem -> Maybe (Arg a v)
@@ -393,7 +419,7 @@ assignRegisters asm =
       eachArg a@(Var x) = fromMaybe a $ assignRegister (Right x)
       eachArg (Deref _ _) = error "TODO"
       eachArg (Imm x) = Imm x
-   in flip map asm \case
+   in flip fmap asm $ map \case
         Movq a b -> Movq (eachArg a) (eachArg b)
         Movzbqal a -> Movzbqal (eachArg a)
         Addq a b -> Addq (eachArg a) (eachArg b)
@@ -411,7 +437,7 @@ assignRegisters asm =
         Callq x -> Callq x
         Retq -> Retq
 
-patchInstructions :: [Asm] -> [Asm]
+patchInstructions :: Cfg [Asm] -> Cfg [Asm]
 patchInstructions =
   let patch2 :: (Arg a Aint -> Arg b Aint -> Asm) -> Arg a Aint -> Arg b Aint -> [Asm]
       patch2 inst (Deref a x) (Deref b y) =
@@ -435,7 +461,7 @@ patchInstructions =
         [ Movq (Reg Rax) (Imm x)
         ] <> patch2 Cmpq a (Reg Rax)
       patchCmpq a b = [Cmpq a b]
-   in concatMap \case
+   in fmap $ concatMap \case
         Movq a b -> patch2 Movq a b
         Movzbqal a -> [Movzbqal a]
         Addq a b -> patch2 Addq a b
@@ -453,8 +479,8 @@ patchInstructions =
         Popq a -> [Popq a]
         Retq -> [Retq]
 
-preludeAndConclusion :: (Int, [Asm]) -> Program
-preludeAndConclusion (fs, asm) =
+preludeAndConclusion :: Int -> Cfg [Asm] -> Program
+preludeAndConclusion fs asm =
   Program
     { globals = ["main", "print_int", "input_int"]
     , asm =
@@ -463,20 +489,22 @@ preludeAndConclusion (fs, asm) =
             , Movq rbp rsp
             , Subq rsp (Imm $ ((fs + 15) `div` 16) * 16)
             ]
-        , ("main_inner",) asm
-        , ("main_conclusion",)
+        ] <> linearizedBlocks <> [
+          ("main_conclusion",)
             [ Movq rsp rbp
             , Popq rbp
             , Retq
             ]
         ]
     }
+  where
+    -- optimizations are possible. also, this step should be moved somewhere else
+    linearizedBlocks = sortBy (flip compare `on` fst) (Map.toList asm.blocks)
 
 compile :: L -> Either TypeCheckerError Program
 compile l = runPureEff . runErrorNoCallStack @TypeCheckerError . runGensym $ do
   ml <- removeComplexOperands l
   anf <- explicateControl ml
   asmvar <- traverse selectInstructions anf
-  _ <- traverse assignHomes asmvar
-  -- pure $ preludeAndConclusion (size, patchInstructions asm)
-  pure (error "TODO")
+  (size, asm) <- assignHomes (assignRegisters asmvar)
+  pure $ preludeAndConclusion size (patchInstructions asm)
